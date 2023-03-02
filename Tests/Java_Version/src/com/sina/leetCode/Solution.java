4f1c20d9eb6e16495c37dda002bd39700c3a5cc3
Saman Pordanesh <sina.pordanesh@yahoo.com>
2023-03-02 10:19:00 -0700

    update

--- Tests/Java_Version/src/com/sina/leetCode/Solution.java
+++ Tests/Java_Version/src/com/sina/leetCode/Solution.java
@@ -1,21 +1,63 @@
 package com.sina.leetCode;
 
-import java.beans.PropertyEditorSupport;
-import java.lang.reflect.Array;
+import java.sql.Struct;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class Solution {
-
-    public int rob(int[] nums) {
-        int notRob = 0;
-        int rob = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int current = nums[i] + notRob;
-            notRob = Math.max(notRob, rob);
-            rob = current;
+/*
+ * inputArr, represents the array with inputArr_size (N) elements.
+ */
+public class Solution
+{
+    public static void  funcMatrix(int[][] matrix)
+    {
+        int result = -1;
+        // Write your code here
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+               if (checkInRow(matrix, i, j) && checkInColumn(matrix, i, j)){
+                   result = matrix[i][j];
+               }
+            }
         }
 
-        return Math.max(rob, notRob);
+        System.out.println(result);
+    }
+
+    public static boolean checkInRow(int[][] matrix, int i, int j){
+        int target = matrix[i][j];
+
+        for (int k = 0; k < matrix[i].length; k++) {
+            if (target < matrix[i][k]) return false;
+        }
+        return true;
+    }
+
+    public static boolean checkInColumn(int[][] matrix, int i, int j){
+        int target = matrix[i][j];
+        for (int k = 0; k < matrix.length; k++) {
+            if (target > matrix[k][j]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        // input for matrix
+        int matrix_row = in.nextInt();
+        int matrix_col = in.nextInt();
+        int matrix[][] = new int[matrix_row][matrix_col];
+        for(int idx = 0; idx < matrix_row; idx++)
+        {
+            for(int jdx = 0; jdx < matrix_col; jdx++)
+            {
+                matrix[idx][jdx] = in.nextInt();
+            }
+        }
+
+
+        funcMatrix(matrix);
     }
 }
