94472d97ce42173afd8e8c249324994e6d05838a
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-06-05 16:34:27 -0600

    Create 7_(238) Product of Array Except Self.py

---
+++ LeetCode 75/7_(238) Product of Array Except Self.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        length = len(nums)
+        result = [1] * length
+
+        left = [1] * length
+        right = [1] * length
+
+        for i in range(length-2, -1, -1):
+            right[i] = right[i+1] * nums[i+1]
+        for i in range(1, length):
+            left[i] = left[i-1] * nums[i-1]
+
+        for i in range(length):
+            result[i] = right[i] * left[i]
+        
+        return result
