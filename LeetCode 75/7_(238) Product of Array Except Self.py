7a83c86b706c8dd3e57c17c4799377c0f7c78241
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-06-05 16:44:05 -0600

    Update 7_(238) Product of Array Except Self.py
    faster version
    - two loops
    - two arrays

--- LeetCode 75/7_(238) Product of Array Except Self.py
+++ LeetCode 75/7_(238) Product of Array Except Self.py
@@ -10,12 +10,15 @@ class Solution(object):
         left = [1] * length
         right = [1] * length
 
-        for i in range(length-2, -1, -1):
-            right[i] = right[i+1] * nums[i+1]
         for i in range(1, length):
             left[i] = left[i-1] * nums[i-1]
 
-        for i in range(length):
-            result[i] = right[i] * left[i]
+        right = nums[-1]
+        for i in range(length-2, -1, -1):
+            left[i] *= right 
+            right *= nums[i]
+
+        # for i in range(length):
+        #     result[i] = right[i] * left[i]
         
-        return result
+        return left
