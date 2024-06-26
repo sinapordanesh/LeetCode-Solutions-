2a6077fbedb84c3147b40a2f76403a54e8b4b0ec
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-06-25 19:45:00 -0600

    Create 21_(1207) Unique Number of Occurrences.py

---
+++ LeetCode 75/21_(1207) Unique Number of Occurrences.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort()
+
+        hmap = {}
+        first = arr[0]
+        occurance = 0
+        for i in arr:
+            if i == first:
+                occurance += 1
+            else:
+                hmap[occurance] = 1
+                occurance = 1
+                if occurance in hmap:
+                    return False
+                first = i
+        return True
