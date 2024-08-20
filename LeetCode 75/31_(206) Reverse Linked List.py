f7024d0c4af453cdcddc12e32e77dd9a77b31a4b
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-08-20 17:50:11 -0600

    Create 31_(206) Reverse Linked List.py

---
+++ LeetCode 75/31_(206) Reverse Linked List.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        
+        current = head
+        previous = None
+        
+        while current:
+            next = current.next
+            current.next = previous
+            previous = current 
+            current = next
+
+        return previous
+        
