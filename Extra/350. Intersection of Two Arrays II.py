7b1781e267680ee8436050eda6a1c2c5409ea5d6
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-08-21 13:52:00 -0600

    Update 350. Intersection of Two Arrays II.py
    Simpler version

--- Extra/350. Intersection of Two Arrays II.py
+++ Extra/350. Intersection of Two Arrays II.py
@@ -9,34 +9,43 @@ class Solution(object):
         nums2.sort()
         i, j = 0, 0
         result = []     
-        while i < len(nums1) and j < len(nums2):
+        # while i < len(nums1) and j < len(nums2):
             
-            while nums1[i] > nums2[j]:
-                    j += 1
-                    if j >= len(nums2): break
-            if i >= len(nums1) or j >= len(nums2): break
-            while nums1[i] < nums2[j]:
-                    i += 1
-                    if i >= len(nums1): break
-            if i >= len(nums1) or j >= len(nums2): break
-            if nums1[i] == nums2[j]:
-                count1 = 0
-                count2 = 0
-                collision = nums1[i]
-                while nums1[i] == collision:
-                    count1 += 1
-                    i += 1
-                    if i >= len(nums1): break 
-                while nums2[j] == collision:
-                    count2 += 1
-                    j += 1
-                    if j >= len(nums2): break 
-                if count1 == count2:
-                    repeating = count1
-                else:
-                    repeating = min(count1, count2)
-                for i in range(repeating):
-                    result.append(collision)
+        #     while nums1[i] > nums2[j]:
+        #             j += 1
+        #             if j >= len(nums2): break
+        #     if i >= len(nums1) or j >= len(nums2): break
+        #     while nums1[i] < nums2[j]:
+        #             i += 1
+        #             if i >= len(nums1): break
+        #     if i >= len(nums1) or j >= len(nums2): break
+        #     if nums1[i] == nums2[j]:
+        #         count1 = 0
+        #         count2 = 0
+        #         collision = nums1[i]
+        #         while nums1[i] == collision:
+        #             count1 += 1
+        #             i += 1
+        #             if i >= len(nums1): break 
+        #         while nums2[j] == collision:
+        #             count2 += 1
+        #             j += 1
+        #             if j >= len(nums2): break 
+        #         if count1 == count2:
+        #             repeating = count1
+        #         else:
+        #             repeating = min(count1, count2)
+        #         for i in range(repeating):
+        #             result.append(collision)
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                result.append(nums1[i])
+                i += 1
+                j += 1
                     
         return result
 
