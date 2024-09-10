e643b42639f175f931759aaf2f01faf85e9c1f7b
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-09-09 20:26:21 -0600

    Create 45_(1466) Reorder Routes to Make All Paths Lead to the City Zero.py

---
+++ LeetCode 75/45_(1466) Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minReorder(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+        
+        self.edges = { (a, b) for a, b in connections}
+        self.neighbors = { city:[] for city in range(n)}
+        self.visit = set()
+        self.change = 0
+
+        for a, b in connections:
+            self.neighbors[a].append(b)
+            self.neighbors[b].append(a)
+
+        def dfs(city):
+            # nonlocal edges
+
+            for neighbor in self.neighbors[city]:
+                if neighbor in self.visit: 
+                    continue
+                
+                if (neighbor, city) not in self.edges:
+                    self.change += 1
+                
+                self.visit.add(neighbor)
+                dfs(neighbor)
+        
+        self.visit.add(0)
+        dfs(0)
+        return self.change
