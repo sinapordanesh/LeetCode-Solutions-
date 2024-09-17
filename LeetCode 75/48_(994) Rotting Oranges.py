fcc9fba735012685de419ffb13078d2e94fd7e64
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-09-16 18:14:30 -0600

    Update 48_(994) Rotting Oranges.py

--- LeetCode 75/48_(994) Rotting Oranges.py
+++ LeetCode 75/48_(994) Rotting Oranges.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution(object):
     def orangesRotting(self, grid):
         """
@@ -5,34 +7,32 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid), len(grid[0])
+        fresh = 0
         start = []
-        
-        if not any(1 in row for row in grid):
-            return 0
+        q = deque()
 
         for i in range(m):
-            if 2 in grid[i]:
-                for j in range(n):
-                    if grid[i][j] == 2: start.append((i, j, 0))
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh +=1
+                elif grid[i][j] == 2:
+                    q.append((i, j))
+        if fresh == 0:
+            return 0
 
         dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        q = collections.deque(start)
-        
         time = 0
-        while q:
-            i, j, curr = q.popleft()
-            exe = False
-            for dx, dy in dirs:
-                x, y = i+dx, j+dy
-                if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
-                    exe = True
-                    grid[x][y] = 2
-                    q.append((x, y, curr+1))
-            if time < curr:
-                time += 1
 
-        if any(1 in row for row in grid):
-            return -1
-        else: 
-            return time
-        
+        while q:
+            size = len(q)
+            for _ in range(size):
+                i, j = q.popleft()
+                for dx, dy in dirs:
+                    x, y = i+dx, j+dy
+                    if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                        grid[x][y] = 2
+                        fresh -= 1
+                        q.append((x, y))
+            time += 1
+
+        return time - 1 if fresh == 0 else -1
