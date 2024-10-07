919db0816d4879e7c3d23d497fbb757857ab1317
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-10-07 15:52:55 -0600

    Update 55_(162) Find Peak Element.py

--- LeetCode 75/55_(162) Find Peak Element.py
+++ LeetCode 75/55_(162) Find Peak Element.py
@@ -4,26 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
-            return 0
+        n = len(nums)
+        
+        if n == 1: return 0
+        if nums[n-1] > nums[n-2] : return n-1
             
-        lo, hi = 0, len(nums)-1
+        lo, hi = 0, n-1
         while lo <= hi: 
             m = (lo+hi)/2
-            if m == 0:
-                if nums[m] > nums[m+1]:
-                    return m
-                else: 
-                    lo = m+1
-            elif m == len(nums) - 1:
-                if nums[m] > nums[m-1]:
-                    return m
-                else: 
-                    hi = m
-            else:
-                if nums[m] > nums[m-1] and nums[m] > nums[m+1]:
-                    return m
-                elif nums[m-1] > nums[m]:
-                    hi = m
-                elif nums[m+1] > nums[m]:
-                    lo = m+1
+            if nums[m] > nums[m-1] and nums[m] > nums[m+1]: return m
+            elif nums[m-1] > nums[m]: hi = m
+            elif nums[m+1] > nums[m]: lo = m+1
